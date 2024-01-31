e309dae7646734c5de55a15f2fb8e91cab526978
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-01-31 19:15:33 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt-get install g++
+pip install tk
+pip install pydub
+pip install opencv-python
+pip install numpy
+pip install wave
+pip install termcolor
+pip install colorama
+chmod +x * EDISC.py
+chmod +x * EDIS.py
+chmod +x activation.c++
